0bdad7e1d8b1dbe2498d5c18d204bef3a9490109
devcharmingk <devcharmingk@naver.com>
2023-01-08 17:04:20 +0900

    initial setting

---
+++ src/main/java/com/devcharmingk/Inhousebulletinboard/entity/Member.java
@@ -0,0 +1,27 @@
+package com.devcharmingk.Inhousebulletinboard.entity;
+
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String contact;
+}
---
+++ src/test/java/com/devcharmingk/Inhousebulletinboard/InHouseBulletinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcharmingk.Inhousebulletinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InHouseBulletinBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
